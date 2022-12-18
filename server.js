2a994cd509844beffdcf3665e77a3221cd562cd8
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-12-17 20:23:11 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Top secret',
+  cookie: {
+
+
+
+
+  },
+  resave: false,
+  saveUniitialized: true,
+  store: new SequelizeStore({
+    db:sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now Listening'));
+})
