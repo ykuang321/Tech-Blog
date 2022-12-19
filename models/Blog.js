a68695901d41b251146cd86ac37fbecc7901afd3
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-12-19 15:45:28 -0600

    add css, add missing packages, add seeds

--- models/Blog.js
+++ models/Blog.js
@@ -18,6 +18,11 @@ Blog.init(
     description: {
       type: DataTypes.STRING,
     },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 const sess = {
   secret: 'Top secret',
   cookie: {
