889dfabd35c2acc7dfff89e8e723b3dc9f5087f9
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-12-24 21:28:07 -0600

    final commit

--- public/js/blog.js
+++ public/js/blog.js
@@ -14,7 +14,7 @@ const newFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to create blog');
     }
